7c74583101cfc11266af9772f1c33ee1faaecd1e
crisu <christian.ahlfors@eng.tamk.fi>
2019-02-12 18:24:38 +0200

    git

---
+++ text.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -f texti.txt ]
+then
+	echo tiedostoa ei ole: luodaan.
+	touch texti.txt
+fi
+echo what text you want to append
+read TEXT
+echo "$TEXT" >> texti.txt
+echo $TEXT appended
+
+tail texti.txt
+
+function add_text() {
+echo what text you want to append
+read TEXT
+echo "$TEXT" >> texti.txt
+echo $TEXT appended
+}
+
+echo "do you want to add more text? (y/n)"
+read VAL
+
+if [ $VAL = "y" ];
+then
+	add_text
+fi
+
+if [ $VAL = "n" ];
+then 
+	exit 0
+fi
