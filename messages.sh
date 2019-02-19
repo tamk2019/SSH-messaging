9ff895a0cf04534a4a439e5315f2870b82f1e403
Juho Lahti <juho.lahti@eng.tamk.fi>
2019-02-19 18:22:33 +0200

    added IP to message

--- messages.sh
+++ messages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+SSH_IP=${SSH_CONNECTION%% *}
 touch messages.txt
 cat -n messages.txt
 echo Write a message:
-read message
-echo $message >> messages.txt
+read message 
+echo $SSH_IP: $message >> messages.txt
